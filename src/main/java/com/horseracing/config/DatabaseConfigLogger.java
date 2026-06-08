4c29b59929cc3246cebc7897b68bda4befcfe8b4
Huynh Van Kha <huynhvankha100306@gmail.com>
2026-06-08 16:01:48 +0700

    create config

---
+++ src/main/java/com/horseracing/config/DatabaseConfigLogger.java
@@ -0,0 +1,21 @@
+package com.horseracing.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class DatabaseConfigLogger implements CommandLineRunner {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dbUsername;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("DATABASE CONFIGURATION DIAGNOSTICS:");
+        System.out.println("JDBC URL: " + dbUrl);
+        System.out.println("DB Username: " + dbUsername);
+    }
+}
