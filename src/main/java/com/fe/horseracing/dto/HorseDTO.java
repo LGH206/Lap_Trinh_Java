0766fe55cb6fc8570af57223f1dc10a9cfc68dff
Huynh Van Kha <huynhvankha100306@gmail.com>
2026-06-15 14:07:18 +0700

    Create DTO Horse

---
+++ src/main/java/com/fe/horseracing/dto/HorseDTO.java
@@ -0,0 +1,80 @@
+package com.fe.horseracing.dto;
+
+public class HorseDTO {
+
+    private int horseId;
+    private String name;
+    private String breed;
+    private int age;
+    private String healthStatus;
+    private String achievements;
+    private int ownerId;
+    private String status;
+
+    public HorseDTO() {
+    }
+
+    public int getHorseId() {
+        return horseId;
+    }
+
+    public void setHorseId(int horseId) {
+        this.horseId = horseId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHealthStatus() {
+        return healthStatus;
+    }
+
+    public void setHealthStatus(String healthStatus) {
+        this.healthStatus = healthStatus;
+    }
+
+    public String getAchievements() {
+        return achievements;
+    }
+
+    public void setAchievements(String achievements) {
+        this.achievements = achievements;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
